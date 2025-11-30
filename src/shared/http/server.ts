cfe8cc8197430dfbb7ac57b9d8a5ec7249ea906f
Reinaldo Ferreira <reinaldo.mferreira@yahoo.com.br>
2025-11-30 15:06:18 -0300

    feat: lint, configs and database

---
+++ src/shared/http/server.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+import cors from "cors";
+
+// Como é o arquivo index que possui routes não é necessário colocar "./routes/index.ts"
+import routes from "./routes";
+import ErrorHandleMiddleware from "@shared/middlewares/ErrorHandleMiddleware";
+import { AppDataSource } from "@shared/typeorm/data-source";
+import { error } from "console";
+
+AppDataSource.initialize()
+  .then(async () => {
+    const app = express();
+
+    app.use(cors());
+    app.use(express.json());
+
+    app.use(routes);
+    app.use(ErrorHandleMiddleware.handleError);
+    console.log("Connect to the database! 🏆");
+    app.listen(3333, () => {
+      console.log("Server started on port 3333! 🏆");
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
