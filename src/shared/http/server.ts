764eb96536d1d1e9278d11803f7ac8c190d0e199
Reinaldo Ferreira <reinaldo.mferreira@yahoo.com.br>
2025-12-02 16:05:55 -0300

    feat: user migrations

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import cors from "cors";
+import { errors } from "celebrate";
 
 // Como é o arquivo index que possui routes não é necessário colocar "./routes/index.ts"
 import routes from "./routes";
@@ -16,6 +17,7 @@ AppDataSource.initialize()
     app.use(express.json());
 
     app.use(routes);
+    app.use(errors());
     app.use(ErrorHandleMiddleware.handleError);
     console.log("Connect to the database! 🏆");
     app.listen(3333, () => {
