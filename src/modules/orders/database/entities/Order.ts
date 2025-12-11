67e96fac4fc486ddcfb970f15ded5b5261dce9ee
Reinaldo Ferreira <reinaldo.mferreira@yahoo.com.br>
2025-12-11 15:54:28 -0300

    feat: entity orders_products

--- src/modules/orders/database/entities/Order.ts
+++ src/modules/orders/database/entities/Order.ts
@@ -1,5 +1,14 @@
 import { Customer } from "@modules/customers/database/entities/Customer";
-import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { OrdersProducts } from "./OrdersProducts";
 
 @Entity("orders")
 export class Order {
@@ -7,13 +16,17 @@ export class Order {
   id: number;
 
   @ManyToOne(() => Customer)
-  @JoinColumn({name: 'customer_id'})
-  customer: Customer
+  @JoinColumn({ name: "customer_id" })
+  customer: Customer;
+
+  @OneToMany(() => OrdersProducts, (order_products) => order_products.order, {
+    cascade: true,
+  })
+  order_products: OrdersProducts[];
 
   @CreateDateColumn()
   created_at: Date;
 
-   @UpdateDateColumn()
+  @UpdateDateColumn()
   updated_at: Date;
 }
-
