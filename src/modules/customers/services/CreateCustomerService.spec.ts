7e04c63c1bc3cb55ecf23684c7c8be8ff0f33bcf
Reinaldo Ferreira <reinaldo.mferreira@yahoo.com.br>
2025-12-24 19:40:36 -0300

    feat: primeiro teste em customer usando jest

---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,8 @@
+describe("CreateCustomerService", () => {
+  it("should be able to create a new customer", () => {
+    expect(5 + 5).toBe(10);
+  });
+  it("should not be able to create a new customer with email tha is already in use", () => {
+    expect(5 + 5).toBe(10);
+  });
+});
