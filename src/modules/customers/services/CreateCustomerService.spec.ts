39e45dbf1ae02a4f9a1a56cf339d920ecaf50c9e
Reinaldo Ferreira <reinaldo.mferreira@yahoo.com.br>
2025-12-29 14:25:26 -0300

    feat: Finalizando os testes do CreateCustomerService

--- src/modules/customers/services/CreateCustomerService.spec.ts
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from "@shared/errors/AppErrors";
 import FakeCustomersRepository from "../domain/repositories/fakes/FakeCustomerRepositories";
 import CreateCustomerService from "./CreateCustomerService";
 
@@ -13,6 +14,23 @@ describe("CreateCustomerService", () => {
 
     expect(customer).toHaveProperty("id");
     expect(customer.email).toBe("john@gmail.com");
+  });
+
+  it("Should not be able to create a new customer with email that is already in use", async () => {
+    const fakeCustomersRepository = new FakeCustomersRepository();
+    const createCustomer = new CreateCustomerService(fakeCustomersRepository);
+
+    await createCustomer.execute({
+      name: "John Doe",
+      email: "john@gmail.com",
+    });
+
+  await expect(
+    createCustomer.execute({
+      name: "John Doe",
+      email: "john@gmail.com",
+    }),
+  ).rejects.toBeInstanceOf(AppError)
   });
 
 });
